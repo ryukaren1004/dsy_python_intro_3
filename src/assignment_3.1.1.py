2ade55e2a15efbb6094c88a2679397ad7f3eb0b0
Karen Ryu <karenryu@Karens-MacBook.local>
2019-03-12 16:12:25 -0500

    Karen Ryu

--- src/assignment_3.1.1.py
+++ src/assignment_3.1.1.py
@@ -1,14 +1,23 @@
 largest = None
 smallest = None
+
 while True:
     num = input("Enter a number: ")
     if num == "done" : break
 
     try:
         ## write your code here
+        num = int(num)
 
+        if largest is None or num > largest:
+            largest = num
+        if smallest is None or num < smallest:
+            smallest = num
     except:
         ## write your code here
+        print("Invalid number")
+        continue
 
 print("Maximum", largest)
+print("Minimun", smallest)
 ## write another print statement here
